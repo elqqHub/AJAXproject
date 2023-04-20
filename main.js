994f98812ab36b564c02d511f1028eef96a621df
ELIAS SAUNA-AHO <elias.saunaaho@gmail.com>
2023-04-20 22:30:29 +0300

    Adjustments and tweaks to the program

--- main.js
+++ main.js
@@ -18,6 +18,21 @@ function getTrainData() {
       .catch(error => console.log(error));
   }
 
-  function showStations() {
-    alert("HKI = Helsinki\nKLH = Kauklahti\nKE = Kerava\nKKN = Kirkkonummi\nSTI = Siuntio\nRI = Riihimäki")
-  }
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+function openWiki() {
+    window.open("https://fi.wikipedia.org/wiki/Luettelo_Suomen_junatyypeist%C3%A4"); 
+}
